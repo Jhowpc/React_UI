ebf884c00e5e066806eed1cc921ee893cf9222c5
Jonathan Sampaio <jonathan.silva97@hotmail.com>
2024-05-19 15:27:54 -0300

    Drawers adicionados e imports corrigidos

--- modulo10/template-materialui/src/pages/Login/index.jsx
+++ modulo10/template-materialui/src/pages/Login/index.jsx
@@ -1,9 +1,7 @@
-import React, {useState, useEffect} from 'react';
-import { FormControl, InputLabel, Input, FormHelperText, Button, Box, Drawer, List, Divider, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, TextField, Checkbox, FormGroup, FormControlLabel, Grid, Typography, Link, Avatar } from '@mui/material';
+import {React, useState} from 'react';
+import { FormControl, Button, Paper, TextField, Checkbox, FormGroup, FormControlLabel, Grid, Typography, Link, Avatar } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login';
-import {MoveToInbox, Mail, Padding} from '@mui/icons-material';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
-
 
 
 const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
@@ -11,6 +9,7 @@ const Login = () => {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
   const btnStyle={margin: '8px 0'}
+  
   return (
     <Grid align= 'center'>
       <Paper elevation={3} style={paperStyle}>
@@ -26,9 +25,9 @@ const Login = () => {
               <Button variant="contained" style={btnStyle} fullWidth><LoginIcon/></Button>
             </FormGroup>
             <Typography>
-            <Link href="https://www.youtube.com/watch?v=AUub_yVjjac">Esqueceu a senha?</Link>
+            <Link a href="https://www.youtube.com/watch?v=AUub_yVjjac" target='_blank'>Esqueceu a senha?</Link>
             </Typography>
-            <Typography> Não possui conta?
+            <Typography> Não possui uma conta?
             <Link href="#"> Registre-se</Link>
             </Typography>
         </FormControl>
